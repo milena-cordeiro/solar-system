acc28855224f32bf66b0cd1b6550a82ed3f85315
Milena Cordeiro <mcs9823@gmail.com>
2023-03-30 18:25:12 -0300

    corrigindo imports em arquivos errados

--- src/components/Missions.jsx
+++ src/components/Missions.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Title from './Title';
 import MissionCard from './MissionCard';
 import missions from '../data/missions';
+import '../estilos/Missions.css';
 
 export default class Missions extends Component {
   render() {
