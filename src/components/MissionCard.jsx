52e7a256a1c6528906d7279b816b0951cd0aa980
Milena Cordeiro <mcs9823@gmail.com>
2023-03-30 19:34:02 -0300

    alterando estilos dos componentes

--- src/components/MissionCard.jsx
+++ src/components/MissionCard.jsx
@@ -1,11 +1,12 @@
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
+import '../estilos/MissionCard.css';
 
 class MissionCard extends Component {
   render() {
     const { name, year, country, destination } = this.props;
     return (
-      <div data-testid="mission-card">
+      <div className="page__missionCard" data-testid="mission-card">
         <p data-testid="mission-name">{name}</p>
         <p data-testid="mission-year">{year}</p>
         <p data-testid="mission-country">{country}</p>
