16d33f1898a62149f263dfd9544ce68a62e4583d
Milena Cordeiro <mcs9823@gmail.com>
2023-03-30 17:07:33 -0300

    Criando uma pasta na qual irão os arquivos css de cada component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
+import '../estilos/Header.css';
 
 export default class Header extends Component {
   render() {
     return (
-      <header>
+      <header className="top__header">
         <h1>Sistema Solar</h1>
       </header>
     );
