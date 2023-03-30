c8cfe74404ebe98626fdb493d06cfdab3d4a4450
Milena Cordeiro <mcs9823@gmail.com>
2023-03-30 16:54:22 -0300

    Requisito 2 - excluindo const desnecessária

--- src/components/SolarSystem.jsx
+++ src/components/SolarSystem.jsx
@@ -5,12 +5,11 @@ import planets from '../data/planets';
 
 export default class SolarSystem extends Component {
   render() {
-    const dataPlanets = planets;
     return (
       <div data-testid="solar-system">
         <Title headline="Planetas" />
         {
-          dataPlanets.map(({ name, image }) => (
+          planets.map(({ name, image }) => (
             <PlanetCard key={ name } planetName={ name } planetImage={ image } />
           ))
         }
