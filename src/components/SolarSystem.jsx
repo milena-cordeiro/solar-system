6b4c389c2921210e4aa841203926e12fc9e6e626
Milena Cordeiro <mcs9823@gmail.com>
2023-03-30 18:23:02 -0300

    estilizando componente SolarSystem

--- src/components/SolarSystem.jsx
+++ src/components/SolarSystem.jsx
@@ -2,11 +2,12 @@ import React, { Component } from 'react';
 import Title from './Title';
 import PlanetCard from './PlanetCard';
 import planets from '../data/planets';
+import '../estilos/SolarSystem.css';
 
 export default class SolarSystem extends Component {
   render() {
     return (
-      <div data-testid="solar-system">
+      <div className="page__solarSystem" data-testid="solar-system">
         <Title headline="Planetas" />
         {
           planets.map(({ name, image }) => (
